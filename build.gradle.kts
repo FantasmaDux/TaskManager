56731b3f03561afc49beb701cb1e1079ce76896d
FantasmaDux <gdgdgdgdsd@mail.ru>
2026-03-07 23:18:00 +0300

    add all dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.12-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.fantasmaDux"
+version = "0.0.1-SNAPSHOT"
+description = "Simple task manager for small teams with export and report "
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/snapshot") }
+	maven { url = uri("https://jaspersoft.jfrog.io/jaspersoft/third-party-ce-artifacts/") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// GWT Integration
+	implementation ("org.jadice.gwt.spring:gwt-spring-boot-starter:2.2.6")
+
+	// Apache POI
+	implementation ("org.apache.poi:poi:5.3.0")
+	implementation ("org.apache.poi:poi-ooxml:5.3.0")
+
+	// JasperReports
+	implementation ("net.sf.jasperreports:jasperreports:6.20.0")
+	implementation ("net.sf.jasperreports:jasperreports-fonts:6.20.0")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ readme.md
@@ -0,0 +1,37 @@
+# Приложение TaskManager
+![GitHub Created At](https://img.shields.io/github/created-at/FantasmaDux/shoppingCart?color=bright-green&style=flat-square)
+![Java 21](https://img.shields.io/badge/Java-17-brightgreen?style=flat-square&logo=java&logoColor=white)
+![Swagger](https://img.shields.io/badge/Swagger-3-brightgreen?style=flat-square&logo=swagger&logoColor=white)
+![Spring Boot](https://img.shields.io/badge/Spring%20Boot-3-brightgreen?style=flat-square&logo=springboot&logoColor=white)
+![Spring Security](https://img.shields.io/badge/Spring%20Security-6-brightgreen?style=flat-square&logo=springsecurity&logoColor=white)
+![PostgreSQL](https://img.shields.io/badge/PostgreSQL-16-brightgreen?style=flat-square&logo=postgresql&logoColor=white)
+
+
+
+## Содержание
+- [Технологии](#технологии)
+- [Безопасность](#безопасность)
+- [Начало работы](#начало-работы)
+- [API](#api)
+- [Лицензия](#лицензия)
+
+## Технологии
+- [Java 17](https://www.java.com/)
+- [Swagger](https://swagger.io/)
+- [Spring Boot 3](https://spring.io/projects/spring-boot)
+- [PostgreSQL](https://www.postgresql.org/)
+- [Gradle](https://gradle.org/)
+  ![GWT]()
+  ![Apache POI]()
+  ![JasperReports]()
+
+### Запуск проекта
+
+
+## API
+### Swagger
+Swagger доступен по адресу http://localhost:8080/swagger-ui/index.html
+
+## Лицензия
+
+[MIT](https://choosealicense.com/licenses/mit/)
